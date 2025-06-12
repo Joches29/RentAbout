f1e923ffefd3c03f3eaab14207864f6b3efed572
Varus(Fofo) <jochaveshe@est.utn.ac.cr>
2025-06-12 15:00:52 -0600

    Se crean las clases Technician y Person y se modifica la clase User
    Se agrega la clase Person con las variables generalizada de Id, Name, Email, luego se crea la clase Technician y User generalizando las variables especificas de esas clases

--- src/Persons/User.java
+++ src/Persons/User.java
@@ -2,44 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Users;
+package Persons;
 
 
-public class User {
+public class User extends Person {
     
-    private String name;
-    private int id;
     private String phone;
-    private String email;
-    
     public static final String phoneRegex = "^[245678]\\d{7}$";
-    public static final String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
 
     public String getPhone() {
         return phone;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
+    
     public void setPhone(String phone) {
         if (validatePhone(phone)){
             this.phone = phone;
-        }
-    }
-
-    public void setEmail(String email) {
-        if (validateEmail(email)){
-            this.email = email;
         }
     }
     
@@ -47,24 +24,17 @@ public class User {
         return phone.matches(phoneRegex);
     }
     
-    private boolean validateEmail(String email){
-        return email.matches(emailRegex);
-    }
-    
-    public User (int id, String name, String phone, String email){
-        this.id = id;
-        this.name = name;
+    public User (int id, String name, String email, String phone){
+        //super llama a la clase base (person) para utilizar el constructor de person
+        super(id, name, email);
         if (validatePhone(phone)){
             this.phone = phone;
-        }
-        if (validateEmail(email)){
-            this.email = email;
         }
     }
     
     @Override
     public String toString(){
-        return "User id: " + id + " User name: " + name + " User phone: " + phone + " User email: " + email;
+        return "User: " + super.toString() + " Phone: " + phone;
     }
     
     
