736fadefac454f253ae27ad7fcd76fb7c015c39f
Joches29 <jochaveshe@est.utn.ac.cr>
2025-06-16 16:06:54 -0600

    Actualizacion BicycleList
    Se programa los metodos add, delete, sort y search en la clase BicycleList

--- src/Bicycles/BicycleList.java
+++ src/Bicycles/BicycleList.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -14,21 +14,52 @@ public class BicycleList implements Lists<Bicycle>{
 
     @Override
     public boolean add(Bicycle t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max ; i++) {
+            if(list[i]==null){
+                list[i]=t;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean delete(Bicycle t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            if(list[i]==t){
+                list[i]=null;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void sort() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < max-1; j++) {
+                if(list[j]!=null && list[j+1]!=null && list[j].getId() > list[j+1].getId()){
+                    Bicycle temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public Bicycle search(Object id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //convierte el objeto a int, pero convirtiendo el objeto primero a string
+        int idt = Integer.parseInt(id.toString());
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            if(list[i].getId()==idt){
+                return list[i];
+            }
+        }
+        return null;
     }
 }
