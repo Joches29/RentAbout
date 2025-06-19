bbfe2ebe41c5b2efe6bf80db9b217458d550568e
Joches29 <jochaveshe@est.utn.ac.cr>
2025-06-19 10:40:54 -0600

    Creacion clase
    Se agrega la clase UserList con 4 metodos, add, delete, sort y search para realizar diferentes acciones

--- src/Persons/UserList.java
+++ src/Persons/UserList.java
@@ -12,25 +12,59 @@ import RentAbouts.Lists;
  */
 public class UserList implements Lists<User>{
     private User list[];
+    
+    public UserList(){
+        this.list = new User[100];
+    }
 
     @Override
     public boolean add(User t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max ; i++) {
+            if(list[i]==null){
+                list[i]=t;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean delete(User t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            if(list[i]==t){
+                list[i]=null;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void sort() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < max-1; j++) {
+                if(list[j]!=null && list[j+1]!=null && list[j].getId() > list[j+1].getId()){
+                    User temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public User search(Object id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int idt = Integer.parseInt(id.toString());
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            if(list[i]!=null && list[i].getId()==idt){
+                return list[i];
+            }
+        }
+        return null;
     }
     
 }
