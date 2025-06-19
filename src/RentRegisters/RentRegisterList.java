474f16b4ab06fae71e64885b1f6150f9028f1565
Joches29 <jochaveshe@est.utn.ac.cr>
2025-06-19 10:41:27 -0600

    Creacion clase
    Se agrega la clase RentRegisterList con 4 metodos, add, delete, sort y search para realizar diferentes acciones

--- src/RentRegisters/RentRegisterList.java
+++ src/RentRegisters/RentRegisterList.java
@@ -13,24 +13,58 @@ import RentAbouts.Lists;
 public class RentRegisterList implements Lists<RentRegister> {
     private RentRegister list[];
 
+    public RentRegisterList() {
+        this.list = new RentRegister[100];
+    }
+
     @Override
     public boolean add(RentRegister t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max ; i++) {
+            if(list[i]==null){
+                list[i]=t;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean delete(RentRegister t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            if(list[i]==t){
+                list[i]=null;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void sort() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       int max = list.length;
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < max-1; j++) {
+                if(list[j]!=null && list[j+1]!=null && list[j].getId() > list[j+1].getId()){
+                    RentRegister temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public RentRegister search(Object id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int idt = Integer.parseInt(id.toString());
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            if(list[i]!=null && list[i].getId()==idt){
+                return list[i];
+            }
+        }
+        return null;
     }
     
 }
